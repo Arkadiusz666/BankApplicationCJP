1c0305fe5f5aa9e384db10476634e33baeaf5e63
arkadiusz666 <arkadiusz666@gmail.com>
2016-05-09 22:15:09 +0200

    Merge branch 'master' of C:\Users\arkad_000\IdeaProjects\CJP\BankApplicationCJP with conflicts.

--- src/main/java/com/luxoft/CJP/April16/akrzos/bankapp/services/BankServiceImplementation.java
+++ src/main/java/com/luxoft/CJP/April16/akrzos/bankapp/services/BankServiceImplementation.java
@@ -6,7 +6,6 @@ import com.luxoft.CJP.April16.akrzos.bankapp.accounts.Account;
 import com.luxoft.CJP.April16.akrzos.bankapp.exceptions.OverdraftLimitExceededException;
 
 import java.io.*;
-import java.util.stream.Stream;
 
 /**
  * Created by akrzos on 2016-04-12.
@@ -21,7 +20,6 @@ public class BankServiceImplementation implements BankService {
     public void removeClient(Bank bank, Client client) {
         bank.removeClient(client);
         //System.out.println("Client removed from the bank");
-
     }
 
     public void addAccount(Bank bank, Client client, Account account) {
@@ -36,14 +34,6 @@ public class BankServiceImplementation implements BankService {
         client.removeAccount(account);
         //System.out.println("Konto usuniete");
     }
-//    public void saveClientToStream(Client client, ObjectOutputStream oos) {
-//        //TODO
-//        todo
-//    }
-//    public void loadClientFromStream(Client client, ObjectOutputStream oos) {
-//        //TODO
-//        todo
-//    }
 
     public void saveClient(Client client) {
         FileOutputStream fos = null;
