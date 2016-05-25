8f6ad7eda917ad5052122a656ece576417be0319
akrzos <arkadiusz666@gmail.com>
2016-05-25 15:06:53 +0200

    Ex10 begun
    Ex9 Databases -
    Almost all DAO commands implemented
    Bank commander fucntionality mostly implemented
    Updating values after depositing etc implemented
    fixed selecting accounts from commander
    
    all substask up to 8 completed
    Update db on adddint client via Commander!!

--- src/main/java/com/luxoft/CJP/April16/akrzos/bankapp/database/annotation/NoDB.java
+++ src/main/java/com/luxoft/CJP/April16/akrzos/bankapp/database/annotation/NoDB.java
@@ -1,7 +1,11 @@
 package com.luxoft.CJP.April16.akrzos.bankapp.database.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * Created by akrzos on 2016-05-23.
  */
+@Target(ElementType.FIELD)
 public @interface NoDB {
 }
---
+++ src/main/java/com/luxoft/CJP/April16/akrzos/bankapp/tests/ReflectionTest.java
@@ -0,0 +1,23 @@
+package com.luxoft.CJP.April16.akrzos.bankapp.tests;
+
+import org.junit.Test;
+
+/**
+ * Created by akrzos on 2016-05-25.
+ */
+public class ReflectionTest {
+    /**
+     * This method should analyze the fields o1 and o2.
+     * It should compare all the fields with the help of equals,
+     * except for those fields that are marked with an annotation
+     *  @NoDB
+     * And return true, if all the fields are equal.
+     * Also it should be able to compare the collections.
+     */
+    @Test
+    public static boolean isEquals (Object o1, Object o2) {
+
+        return true;
+    }
+
+}
