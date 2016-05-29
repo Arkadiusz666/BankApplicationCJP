0233cc6ebcd42a4f841ed08e062870d91af9e52a
arkadiusz666 <arkadiusz666@gmail.com>
2016-05-29 14:37:52 +0200

    Excercise 10 - equals method implemented (not tested yet)
    TODO
    The counter monitor is not working
    Ex7 - still no serialization over the network

--- src/main/java/com/luxoft/CJP/April16/akrzos/bankapp/tests/ReflectionTest.java
+++ src/main/java/com/luxoft/CJP/April16/akrzos/bankapp/tests/ReflectionTest.java
@@ -1,6 +1,12 @@
 package com.luxoft.CJP.April16.akrzos.bankapp.tests;
 
+import com.luxoft.CJP.April16.akrzos.bankapp.database.annotation.NoDB;
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by akrzos on 2016-05-25.
@@ -15,9 +21,27 @@ public class ReflectionTest {
      * Also it should be able to compare the collections.
      */
     @Test
-    public static boolean isEquals (Object o1, Object o2) {
+    public static boolean isEqualsExceptForFieldsMarkedasNoDB (Object o1, Object o2) {
+        Field[] fields1 = o1.getClass().getDeclaredFields();
+        Field[] fields2 = o1.getClass().getDeclaredFields();
 
-        return true;
+        ArrayList<Field> filteredFields1 = new ArrayList<>();
+        ArrayList<Field> filteredFields2 = new ArrayList<>();
+
+        for (Field field : fields1) {
+            if (field.getAnnotation(NoDB.class) ==null) {
+                filteredFields1.add(field);
+            }
+        }
+
+        for (Field field : fields2) {
+            if (field.getAnnotation(NoDB.class) ==null) {
+                filteredFields2.add(field);
+            }
+        }
+
+        return Objects.equals(filteredFields1,filteredFields2);
+
     }
 
 }
